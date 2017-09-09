74e71869afe9ca2ef6d2767b1585913ccbbcb29d
VladGoloyan <31705039+VladGoloyan@users.noreply.github.com>
2017-09-09 20:58:05 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,11 @@
+console.log("Hello Github!");
+a=3+4,b=10-2,c=12*3,d=12/3,i=0;
+document.getElementById("p1").innerHTML+=a;
+document.getElementById("p2").innerHTML+=b;
+document.getElementById("p3").innerHTML+=c;
+document.getElementById("p4").innerHTML+=d;
+function a1(){
+	alert("Surprise");
+	i++;
+	console.log(i);
+}
