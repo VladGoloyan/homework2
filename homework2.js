7d5ff8fac53dd0272363d6f665accd975cf75614
VladGoloyan <31705039+VladGoloyan@users.noreply.github.com>
2017-09-12 19:01:24 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,5 +1,6 @@
 console.log("Hello Github!");
-a=3+4,b=10-2,c=12*3,d=12/3,i=0;
+const a=3+4,b=10-2,c=12*3,d=12/3;
+i=0;
 document.getElementById("p1").innerHTML+=a;
 document.getElementById("p2").innerHTML+=b;
 document.getElementById("p3").innerHTML+=c;
